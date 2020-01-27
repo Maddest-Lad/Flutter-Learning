e73a1cfd0413ae8beb1d543798a45b0e50e570a3
SamHarris2020 <sam.career@outlook.com>
2020-01-27 09:16:14 -0800

    Prototyped Login Button and Reset Password Button, Didn't Use The Actual Floating Button Class Yet

--- lib/HomePage.dart
+++ lib/HomePage.dart
@@ -7,6 +7,7 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return SafeArea(
         child: Scaffold(
+            //Background Container
             body: Container(
             decoration: new BoxDecoration(
               gradient: new LinearGradient(
@@ -16,27 +17,69 @@ class HomePage extends StatelessWidget {
                     Colors.indigo.shade400,
                     Colors.indigo.shade800
                   ],
-
                   begin: Alignment.topCenter,
                   end: Alignment.bottomCenter,
                   stops: [0.0, 0.15, 0.3, 1.0],
                   tileMode: TileMode.clamp),
             ),
               child: new Column(
+
+                // List of Widgets In Use
                 children: <Widget>[
+
+                  // Appbar At The Top
                   GradientAppBar("Team App"),
 
+                  // Separator Bar
                   Container(
                     constraints:
                     BoxConstraints.expand(height: 5, width: double.infinity),
                     color: HexColor('FF1F22'),
                   ),
 
+                  // Logo Display
                   Container(
-                    padding: EdgeInsets.only(top: 10),
+                    padding: EdgeInsets.only(top: 20, bottom: 20),
                     constraints: BoxConstraints.expand(height: 300),
                     alignment: Alignment.center,
                     child: Image.asset("assets/852_Logo.png", fit: BoxFit.cover),
+                  ),
+
+                  // Login Box
+                  Container(
+                    constraints: BoxConstraints.expand(
+                        width: MediaQuery.of(context).size.width/2,
+                        height: 80),
+                    decoration: BoxDecoration(
+                      color: HexColor('F26622'),
+                      border: Border.all(
+                          color: HexColor('150F6D'),
+                          width: 5,
+                          style: BorderStyle.solid
+                      ),
+                      borderRadius: BorderRadius.circular(20.0),
+                    ),
+
+                    child: Center (
+                      child: Text("Login",
+                          style: const TextStyle(
+                            color: Colors.white,
+                            fontFamily: 'Spectral',
+                            fontWeight: FontWeight.w300,
+                            fontSize: 36.0
+                          )
+                      ),
+                    ),
+                  ),
+
+
+                  Text("Forgot Your Password",
+                    style: const TextStyle(
+                        color: Colors.white,
+                        fontFamily: 'Spectral',
+                        fontWeight: FontWeight.w300,
+                        fontSize: 16.0
+                    )
                   ),
                 ],
               ),
