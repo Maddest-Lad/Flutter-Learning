f0b078c0db182648288b6138f5c37f3b0b58120a
SamHarris2020 <sam.career@outlook.com>
2020-01-28 12:25:44 -0800

    Added Username Bar And Icon

--- lib/HomePage2.dart
+++ lib/HomePage2.dart
@@ -10,14 +10,20 @@ class HomePage2 extends StatelessWidget {
             body: Column(
               children: <Widget>[
                 GradientAppBar("Team 852", Colors.lightBlue, Colors.lightBlue, false),
+                spacingBar(Colors.blueAccent),
+                displayImage("assets/852_Logo_Blue.png"),
+                blankSpacer(10),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: <Widget>[
+                    Icon(Icons.account_circle, color: Colors.indigo, size: 55.0),
 
-                // Separator Bar
-                Container(
-                  constraints:
-                  BoxConstraints.expand(height: 5, width: double.infinity),
-                  color: Colors.blueAccent,
+                    //Username Box
+                    Container(alignment: Alignment.centerLeft, padding: EdgeInsets.only(left: 20), constraints: BoxConstraints.expand(width: 300, height: 50), decoration: BoxDecoration(color: Colors.lightBlue, borderRadius: BorderRadius.circular(45.0), border: Border.all(color: Colors.blueAccent[700], width: 5, style: BorderStyle.solid),),
+                      child: spectralFont("Username:"),
+                    )
+                  ],
                 )
-
               ],
             )
         )
