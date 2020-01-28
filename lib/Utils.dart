bb766cef5a3e9f71d5175d91c134e2bd4a9cfb6e
SamHarris2020 <sam.career@outlook.com>
2020-01-28 12:26:41 -0800

    Added spacingBar(), displayImage(), blankSpacer(), and spectralFont() Methods

--- lib/Utils.dart
+++ lib/Utils.dart
@@ -19,3 +19,39 @@ class HexColor extends Color {
 
   HexColor(final String hexColor) : super(_getColorFromHex(hexColor));
 }
+
+// Returns A Spacer To Go With A AppBar
+Container spacingBar(Color c) {
+  return Container(
+    constraints:
+    BoxConstraints.expand(height: 5, width: double.infinity),
+    color: c,
+  );
+}
+
+// Simplifies Image Display Code
+Container displayImage(String name) {
+  return Container(
+    padding: EdgeInsets.only(top: 20, bottom: 20),
+    constraints: BoxConstraints.expand(height: 300),
+    alignment: Alignment.center,
+    child: Image.asset(name, fit: BoxFit.cover),
+  );
+}
+
+Container blankSpacer(double height) {
+  return Container(
+    height: height,
+  );
+}
+
+Text spectralFont(String text) {
+  return Text(
+      text,
+      style: const TextStyle(
+      color: Colors.white,
+      fontFamily: 'Spectral',
+      fontWeight: FontWeight.bold,
+      fontSize: 16)
+  );
+}
